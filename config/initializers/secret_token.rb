d50349f2291521eb3069bba46612c57d6ccee744
Katherine Whitlock <toroidalcodea@gmail.com>
2013-03-15 17:28:33 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMyCode::Application.config.secret_token = '366d989f31638d007f030dee0efd96f7dbc281d563707a460aa9ec1bae2cd1281a49e2a14c0ccb9438c5fb4281f94f804c2dc7989e686fd46f8ad1cffcf51e6f'

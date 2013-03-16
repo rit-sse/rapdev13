404abaef0aed77810d5e28b4f2b087858f54b28c
Lee Avital <leeavital@gmail.com>
2013-03-16 16:47:28 -0400

    Added the name to the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :profile
   has_many :submissions, order: :position
-  attr_accessible :is_private, :permalink
+  attr_accessible :is_private, :permalink, :name
   validates :permalink,
           format: { with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, :message => "must be a valid URL." }
   validates :is_private, presence: true
